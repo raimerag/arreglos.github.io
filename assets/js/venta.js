69e1ca51a60d6c74de4b98a4ca9fcdce6b007970
raimer aguilar <aguilar.raimer@gmail.com>
2024-10-07 02:20:02 -0300

    first commit

---
+++ assets/js/venta.js
@@ -0,0 +1,55 @@
+let contenedorVentas = document.querySelector("#contenedor-ventas")
+  
+    for(let casas of ventas){
+      let fumar = ''
+      let mascotas = ''
+       
+        if(casas.smoke === false){
+            fumar = ' no se puede fumar'
+        }
+        else{
+            fumar = ' si se puede fumar'
+        }
+
+        if(casas.mascotas === false){
+            mascotas = ' No se permiten mascotas'
+        }
+        else{
+            mascotas = ' Mascotas permitidas'
+        }
+
+        let nuevoDiv = document.createElement("div")
+        nuevoDiv.classList.add("col-md-4","mb-4")
+        nuevoDiv.innerHTML =`<div class="card">
+              <img
+                src=${casas.src}
+                class="card-img-top"
+                alt="Imagen del departamento"
+              />
+              <div class="card-body">
+                <h5 class="card-title">
+                  ${casas.nombre}
+                </h5>
+                <p class="card-text">
+                ${casas.descripcion}
+                </p>
+                <p>
+                  <i class="fas fa-map-marker-alt"></i> ${casas.ubicacion}
+                </p>
+                <p>
+                  <i class="fas fa-bed"></i> ${casas.habitaciones}
+                  <i class="fas fa-bath"></i> ${casas.baños}
+                </p>
+                <p><i class="fas fa-dollar-sign"></i>${casas.costo}</p>
+                <p class="text-danger">
+                  <i class="fas fa-smoking-ban"></i>${fumar}
+                </p>
+                <p class="text-danger">
+                  <i class="fa-solid fa-ban"></i> ${mascotas}
+                </p>
+              </div>
+            </div>
+          </div>`
+            contenedorVentas.appendChild(nuevoDiv)    
+        }
+    
